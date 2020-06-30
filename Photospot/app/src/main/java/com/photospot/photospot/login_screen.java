1aaac9b382a934cdac480494e005c98c884e3006
JustMilan <milandol321@gmail.com>
2020-06-30 13:51:27 +0200

    Create button in main activity to direct to login screen, create login screen without functionalities.

--- Photospot/app/src/main/java/com/photospot/photospot/login_screen.java
+++ Photospot/app/src/main/java/com/photospot/photospot/login_screen.java
@@ -1,19 +1,13 @@
 package com.photospot.photospot;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class login_screen extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void onClick(View view) {
         setContentView(R.layout.login_screen);
     }
 }
