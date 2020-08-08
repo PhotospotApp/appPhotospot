bd7469cfba893eb301112026844dd3735e7a8651
Milan Dol <milandol321@gmail.com>
2020-08-08 13:43:37 +0200

    Firebase Auth working with google sing in

--- Photospot/app/src/main/java/com/photospot/photospot/LoginActivity.java
+++ Photospot/app/src/main/java/com/photospot/photospot/LoginActivity.java
@@ -1,7 +1,5 @@
 package com.photospot.photospot;
 
-import android.accounts.Account;
-import android.app.Activity;
 import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.graphics.drawable.Drawable;
@@ -10,37 +8,36 @@ import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Toast;
-
-import com.firebase.ui.auth.AuthUI;
-import com.firebase.ui.auth.IdpResponse;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-
-import java.util.Arrays;
-import java.util.List;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 import static android.content.ContentValues.TAG;
 
-public class LoginActivity extends Activity {
-    private static final int RC_SIGN_IN = 123;
+public class LoginActivity extends AppCompatActivity {
+
+    static final int GOOGLE_SIGN = 123;
+    GoogleSignInClient mGoogleSignInClient;
+    FirebaseAuth mAuth;
 
     //    Animations
     AnimationDrawable backgroundAnimation;
     Drawable backgroundDrawable;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_screen);
-
-        FirebaseApp.initializeApp(this);
-
 
         //Hiding statusbar
         View decorView = getWindow().getDecorView();
@@ -51,85 +48,80 @@ public class LoginActivity extends Activity {
         backgroundDrawable = findViewById(R.id.root_layout).getBackground();
         Utils.animateGradient(backgroundDrawable, 4000);
 
-        findViewById(R.id.sign_in_button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                createSignInIntent();
-            }
-        });
-    }
+        findViewById(R.id.sign_in_button).setOnClickListener(view -> signInGoogle());
 
-    @Override
-    protected void onStart() {
-        super.onStart();
+        mAuth = FirebaseAuth.getInstance();
 
-        updateUI(FirebaseAuth.getInstance().getCurrentUser());
-    }
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions
+                .Builder()
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
 
-    private void updateUI(FirebaseUser account) {
-        if (account != null) {
-            Intent intent = new Intent(this, MapActivity.class);
-            intent.putExtra("account", account);
-            startActivity(intent);
+        mGoogleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
+
+        if (mAuth.getCurrentUser() != null) {
+            FirebaseUser user = mAuth.getCurrentUser();
+            updateUi(user);
         }
     }
 
-    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
-        try {
-            // Signed in successfully, show authenticated UI.
-            updateUI(FirebaseAuth.getInstance().getCurrentUser());
-        } catch (Exception e) {
-            // The ApiException status code indicates the detailed failure reason.
-            // Please refer to the GoogleSignInStatusCodes class reference for more information.
-            Log.w(TAG, "signInResult:failed code=" + e.getMessage());
-            updateUI(null);
-        }
-    }
-
-    public void createSignInIntent() {
-        // [START auth_fui_create_intent]
-        // Choose authentication providers
-//        List<AuthUI.IdpConfig> providers = Arrays.asList(
-//                new AuthUI.IdpConfig.EmailBuilder().build(),
-//                new AuthUI.IdpConfig.PhoneBuilder().build(),
-//                new AuthUI.IdpConfig.GoogleBuilder().build(),
-//                new AuthUI.IdpConfig.FacebookBuilder().build(),
-//                new AuthUI.IdpConfig.TwitterBuilder().build());
-
-        List<AuthUI.IdpConfig> providers = Arrays.asList(
-                new AuthUI.IdpConfig.GoogleBuilder().build());
-
-        // Create and launch sign-in intent
-        FirebaseApp.initializeApp(this);
-        startActivityForResult(
-                AuthUI.getInstance()
-                        .createSignInIntentBuilder()
-                        .setAvailableProviders(providers)
-                        .build(),
-                RC_SIGN_IN);
-        // [END auth_fui_create_intent]
+    void signInGoogle() {
+        Intent signIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signIntent, GOOGLE_SIGN);
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        System.out.println("In on activity result");
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == GOOGLE_SIGN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn
+                    .getSignedInAccountFromIntent(data);
 
-        if (requestCode == RC_SIGN_IN) {
-            IdpResponse response = IdpResponse.fromResultIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                if (account != null) {
+                    //make request firebase
+                    firebaseAuthWithGoogle(account);
+                    updateUi(mAuth.getCurrentUser());
+                }
 
-            if (resultCode == RESULT_OK) {
-                // Successfully signed in
-                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                handleSignInResult(GoogleSignIn.getSignedInAccountFromIntent(data));
-
-                // ...
-            } else {
-                // Sign in failed. If response is null the user canceled the
-                // sign-in flow using the back button. Otherwise check
-                // response.getError().getErrorCode() and handle the error.
-                // ...
+            } catch (ApiException e) {
+                e.printStackTrace();
             }
+        }
+
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
+        Log.d(TAG, "firebaseAuthWithGoogle: " + account.getId());
+
+        AuthCredential credential = GoogleAuthProvider
+                .getCredential(account.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        Log.d(TAG, "signin succes");
+
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        updateUi(user);
+                    } else {
+                        Log.w(TAG, "sigin failure", task.getException());
+
+                        Toast.makeText(this, "SignIn failed!", Toast.LENGTH_SHORT).show();
+                        updateUi(null);
+                    }
+                });
+    }
+
+    private void updateUi(FirebaseUser user) {
+        if (user != null) {
+
+            Intent intent = new Intent(this, MapActivity.class);
+            intent.putExtra("user", user);
+            startActivity(intent);
+        } else {
+            setContentView(R.layout.login_screen);
         }
     }
 }
