ddb4b01598e43fbabe52232e72a2e3eeb332b7bb
JustMilan <milandol321@gmail.com>
2021-01-22 17:02:15 +0100

    Add profile picture and name

---
+++ Photospot/app/src/main/java/com/photospot/photospot/ProfileActivity.java
@@ -0,0 +1,15 @@
+package com.photospot.photospot;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
